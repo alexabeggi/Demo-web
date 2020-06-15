7461680f98238829cb5ec957d4f420b462daa6b7
alexabeggi <alexabeggi@users.noreply.github.com>
2020-06-15 15:48:41 +0200

    adding functionality with jQuery

---
+++ jq-main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    
+    //---------------OPENING---------------
+
+    $(function() {
+        var pause = 500;
+        setInterval(function() {
+            //$('#opening').fadeOut(3000);
+            $('#opening').slideUp(1700, function(){
+                $('#title').animate({'opacity': "1"}, 700);
+            });
+        },pause);
+    });
+    
+    //---------------SLIDER---------------
+    $(function () {
+        var width = 430;
+        var animationSpeed = 1000;
+        var pause = 3000;
+        var currentSlide = 1;
+        
+        setInterval(function() {
+            $('#slider .slides').animate({'margin-left': '-='+width}, animationSpeed, function (){
+                currentSlide++;
+                if (currentSlide === $('#slider .slide').length){
+                //back to the first slide
+                currentSlide = 1;
+                $('#slider .slides').css('margin-left', 0);
+                }
+            });
+        }, pause);
+
+    });
+
+    //---------------DROPDOWN MENU (HELP)---------------
+    $('#dropdown').hide();
+    $('#help').click(function(){ 
+        $('#dropdown').slideToggle(500);
+        });
+
+
+});
